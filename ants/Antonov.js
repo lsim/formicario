20ae096d3928a0ab1faf4e2847c411ec4e735e85
Lars Ole Simonsen <larsolesimonsen@gmail.com>
2025-07-18 22:31:24 +0200

    Speed up Antonov expansion

--- ants/Antonov.js
+++ ants/Antonov.js
@@ -270,7 +270,7 @@ function adamAnt(squares, antInfo) {
     // When given directions at base, spread evenly around the circle. When exploring from elsewhere, choose a different point on the circle.
     if (brain.x !== 0 || brain.y !== 0) {
       brain.patternSeed += (numSlices / 3) | 0;
-      brain.range += 2;
+      brain.range += 3;
     }
     const radian = getRadian(brain.patternSeed);
     const destX = cos(radian) * brain.range;
